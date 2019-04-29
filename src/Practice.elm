31a5d654673e3ebeb16642a630ea5549cd3f63f3
jaeyson <contact@jaeyson.com>
2019-04-30 04:01:16 +0800

    add/edit feature complete

--- src/Practice.elm
+++ src/Practice.elm
@@ -1,8 +1,171 @@
 module Practice exposing (..)
+import Browser exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+-- Main
+main =
+  Browser.sandbox
+    { init = init
+    , update = update
+    , view = view
+    }
+
 
 
 -- Model
+type Mode
+  = EditMode
+  | ViewMode
 
+type alias Content =
+  { content : String }
+
+type alias Model =
+  { contents : List Content
+  , input : String
+  , mode : Mode
+  }
+
+init : Model
+init =
+  { contents = []
+  , input = ""
+  , mode = ViewMode
+  }
+
+
+
+-- Update
+
+type Msg
+  = Input String
+  | Clear
+  | Add
+  | Edit String
+  | Delete Content
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Clear ->
+      { model | input = ""
+              , mode = ViewMode
+      }
+    Input value ->
+      { model | input = value }
+    Add ->
+      case (String.isEmpty model.input) of
+        True ->
+          { model | input = ""
+                  , mode = ViewMode
+          }
+        False ->
+          add model
+    Edit editValue ->
+      { model | input = editValue
+              , mode = EditMode
+      }
+
+add model =
+  let
+      newContent =
+        Content model.input
+      allContents =
+        newContent :: model.contents
+
+      isDuplicateContent =
+        model.contents
+        |> List.map .content
+        |> List.member model.input
+  in
+      case isDuplicateContent of
+        True ->
+          { model | input = ""
+                  , mode = ViewMode
+          }
+        False ->
+          { model | contents = allContents
+                  , input = ""
+                  , mode = ViewMode
+          }
+
+{--
+edit model =
+  let
+      result =
+        model.contents
+        |> List.map .content
+        |> List.filter (\p -> p == model.input)
+        |> List.head
+
+  in
+      case result of
+        Just value ->
+          { model | input = value
+                  , mode = EditMode
+          }
+        Nothing ->
+          model
+--}
+
+
+
+-- View
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ contentList model
+    , input [ type_ "text"
+            , placeholder "enter content..."
+            , onInput Input
+            , value (if model.input == "" then
+                        ""
+                      else
+                        model.input)
+            ]
+        []
+    , button [ type_ "button", onClick Add ]
+        [ text "Add" ]
+    , button [ type_ "button", onClick Clear ]
+        [ text "Clear" ]
+    , debugSection model
+    ]
+
+contentList : Model -> Html Msg
+contentList model =
+  model.contents
+  |> List.map lists
+  |> ul []
+
+lists : Content -> Html Msg
+lists listContent =
+  li [ onClick (Edit listContent.content) ]
+    [ span [] [ text "Edit - " ]
+    , span [] [ text listContent.content ]
+    ]
+
+debugSection : Model -> Html Msg
+debugSection model =
+  div []
+    [ h3 []
+        [ span [] [ text "Input: " ]
+        , span [] [ text (Debug.toString model.input) ]
+        ]
+    , h3 [] [ text "Content: " ]
+    , h3 [] [ text (Debug.toString model.contents) ]
+    , h3 []
+        [ span [] [ text "Mode: " ]
+        , span [] [ text (Debug.toString model.mode) ]
+        ]
+
+    ]
+
+
+{--
 type alias Model =
   { name : String
   , players : List Player
@@ -18,6 +181,9 @@ init =
   { name = ""
   , players = [ { name = "John Smith"
                 , id = 0
+                }
+              , { name = "John Doe"
+                , id = 1
                 }
               ]
   }
@@ -43,6 +209,25 @@ add newName model =
           { model | players = allPlayers
                   , name = ""
           }
+
+edit : Model -> List Player
+edit model =
+  model.players
+  |> List.map (\editPlayer ->
+    case ( editPlayer.name == model.name ) of
+      True ->
+        setPlayerName model.name editPlayer
+      False ->
+        editPlayer
+  )
+
+setPlayerName : String -> Player -> Player
+setPlayerName newName playerRecord =
+  { playerRecord | name  = newName }
+--}
+
+
+
 
 
 
